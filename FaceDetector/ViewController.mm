405657c60f791878e6a95906977612f9a28e201a
Pau Ballart Godoy <pballart@gmail.com>
2015-05-01 17:14:55 +0200

    initial commit pod OpenCV success app capturing from camera and displaying it toggle camera button implemented

---
+++ FaceDetector/ViewController.mm
@@ -0,0 +1,376 @@
+//
+//  ViewController.m
+//  FaceDetector
+//
+//  Created by Pau Ballart Godoy on 30/4/15.
+//  Copyright (c) 2015 SolArt Apps. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIImage+OpenCV.h"
+#import <AVFoundation/AVFoundation.h>
+
+// Name of face cascade resource file without xml extension
+NSString * const kFaceCascadeFilename = @"haarcascade_frontalface_default";
+
+// Options for cv::CascadeClassifier::detectMultiScale
+const int kHaarOptions =  CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH;
+
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic, strong) AVCaptureSession * captureSession;
+@property (nonatomic, strong) AVCaptureDevice *captureDevice;
+@property (nonatomic, strong) AVCaptureDeviceInput * deviceInput;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+@property (nonatomic, assign) NSInteger camera;
+@end
+
+@implementation ViewController
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    AVCaptureSession * captureSession = [[AVCaptureSession alloc] init];
+    if ([captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720])
+    {
+        [captureSession setSessionPreset:AVCaptureSessionPreset1280x720];
+        NSLog(@"Capturing video at 1280x720");
+    }
+    else
+    {
+        NSLog(@"Could not configure AVCaptureSession video input.");
+    }
+    self.captureSession = captureSession;
+    
+    // Create the preview layer
+    self.videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    [self.videoPreviewLayer setFrame:[UIScreen mainScreen].bounds];
+    self.videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [self.view.layer insertSublayer:self.videoPreviewLayer atIndex:0];
+
+    [self startWithDevicePosition:AVCaptureDevicePositionBack];
+    // Load the face Haar cascade from resources
+    NSString *faceCascadePath = [[NSBundle mainBundle] pathForResource:kFaceCascadeFilename ofType:@"xml"];
+    
+    if (!_faceCascade.load([faceCascadePath UTF8String])) {
+        NSLog(@"Could not load face cascade: %@", faceCascadePath);
+    }
+
+}
+
+- (void)dealloc {
+    [self.captureSession stopRunning];
+}
+
+#pragma mark -
+#pragma mark Public Interface
+- (BOOL)startWithDevicePosition:(AVCaptureDevicePosition)devicePosition {
+    // (1) Find camera device at the specific position
+    AVCaptureDevice * videoDevice = [self cameraWithPosition:devicePosition];
+    if ( !videoDevice ) {
+        NSLog(@"Could not initialize camera at position %ld", (long)devicePosition);
+        return FALSE;
+    }
+    
+    // (2) Obtain input port for camera device
+    NSError * error;
+    AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:videoDevice error:&error];
+    if ( !error ) {
+        [self setDeviceInput:videoInput];
+    } else {
+        NSLog(@"Could not open input port for device %@ (%@)", videoDevice, [error localizedDescription]);
+        return FALSE;
+    }
+    
+    // (3) Configure input port for captureSession
+    if ( [self.captureSession canAddInput:videoInput] ) {
+        [self.captureSession addInput:videoInput];
+    } else {
+        NSLog(@"Could not add input port to capture session %@", self.captureSession);
+        return FALSE;
+    }
+    
+    // (4) Configure output port for captureSession
+    [self addVideoDataOutput];
+    
+    // (5) Start captureSession running
+    [self.captureSession startRunning];
+    
+    return TRUE;
+}
+
+#pragma mark -
+#pragma mark Helper Methods
+- (AVCaptureDevice*)cameraWithPosition:(AVCaptureDevicePosition)position {
+    NSArray * devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for ( AVCaptureDevice * device in devices ) {
+        if ( [device position] == position ) {
+            self.captureDevice = device;
+            return device;
+        }
+    }
+    return nil;
+}
+
+- (void) addVideoDataOutput {
+    // (1) Instantiate a new video data output object
+    AVCaptureVideoDataOutput * captureOutput = [[AVCaptureVideoDataOutput alloc] init];
+    captureOutput.alwaysDiscardsLateVideoFrames = YES;
+    
+    // (2) The sample buffer delegate requires a serial dispatch queue
+    dispatch_queue_t queue;
+    queue = dispatch_queue_create("com.solartapps.opencv", DISPATCH_QUEUE_SERIAL);
+    [captureOutput setSampleBufferDelegate:self queue:queue];
+    //OJU
+    //dispatch_release(queue);
+    
+    // (3) Define the pixel format for the video data output
+    NSString * key = (NSString*)kCVPixelBufferPixelFormatTypeKey;
+    NSNumber * value = [NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA];
+    NSDictionary * settings = @{key:value};
+    [captureOutput setVideoSettings:settings];
+    
+    // (4) Configure the output port on the captureSession property
+    [self.captureSession addOutput:captureOutput];
+}
+
+#pragma mark -
+#pragma mark Sample Buffer Delegate
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
+       fromConnection:(AVCaptureConnection *)connection
+{
+    CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    OSType format = CVPixelBufferGetPixelFormatType(pixelBuffer);
+    CGRect videoRect = CGRectMake(0.0f, 0.0f, CVPixelBufferGetWidth(pixelBuffer), CVPixelBufferGetHeight(pixelBuffer));
+    AVCaptureVideoOrientation videoOrientation = [[[captureOutput connections] objectAtIndex:0] videoOrientation];
+    
+    if (format == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+        // For grayscale mode, the luminance channel of the YUV data is used
+        CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+        void *baseaddress = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
+        
+        cv::Mat mat(videoRect.size.height, videoRect.size.width, CV_8UC1, baseaddress, 0);
+
+        [self processFrame:mat videoRect:videoRect videoOrientation:videoOrientation];
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    }
+    else if (format == kCVPixelFormatType_32BGRA) {
+        // For color mode a 4-channel cv::Mat is created from the BGRA data
+        CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+        void *baseaddress = CVPixelBufferGetBaseAddress(pixelBuffer);
+        
+        cv::Mat mat(videoRect.size.height, videoRect.size.width, CV_8UC4, baseaddress, 0);
+        
+//        [self processFrame:mat videoRect:videoRect videoOrientation:videoOrientation];
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    }
+    else {
+        NSLog(@"Unsupported video format");
+    }
+    
+}
+
+- (void)processFrame:(cv::Mat &)mat videoRect:(CGRect)rect videoOrientation:(AVCaptureVideoOrientation)videOrientation
+{
+    // Shrink video frame to 320X240
+    cv::resize(mat, mat, cv::Size(), 0.5f, 0.5f, CV_INTER_LINEAR);
+    rect.size.width /= 2.0f;
+    rect.size.height /= 2.0f;
+    
+    // Rotate video frame by 90deg to portrait by combining a transpose and a flip
+    // Note that AVCaptureVideoDataOutput connection does NOT support hardware-accelerated
+    // rotation and mirroring via videoOrientation and setVideoMirrored properties so we
+    // need to do the rotation in software here.
+    cv::transpose(mat, mat);
+    CGFloat temp = rect.size.width;
+    rect.size.width = rect.size.height;
+    rect.size.height = temp;
+    
+    if (videOrientation == AVCaptureVideoOrientationLandscapeRight)
+    {
+        // flip around y axis for back camera
+        cv::flip(mat, mat, 1);
+    }
+    else {
+        // Front camera output needs to be mirrored to match preview layer so no flip is required here
+    }
+    
+    videOrientation = AVCaptureVideoOrientationPortrait;
+    
+    // Detect faces
+    std::vector<cv::Rect> faces;
+    
+    _faceCascade.detectMultiScale(mat, faces, 1.1, 2, kHaarOptions, cv::Size(60, 60));
+    
+    // Dispatch updating of face markers to main queue
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        [self displayFaces:faces
+              forVideoRect:rect
+          videoOrientation:videOrientation];
+    });
+}
+
+// Update face markers given vector of face rectangles
+- (void)displayFaces:(const std::vector<cv::Rect> &)faces
+        forVideoRect:(CGRect)rect
+    videoOrientation:(AVCaptureVideoOrientation)videoOrientation
+{
+    NSArray *sublayers = [NSArray arrayWithArray:[self.view.layer sublayers]];
+    int sublayersCount = [sublayers count];
+    int currentSublayer = 0;
+    
+    [CATransaction begin];
+    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+    
+    // hide all the face layers
+    for (CALayer *layer in sublayers) {
+        NSString *layerName = [layer name];
+        if ([layerName isEqualToString:@"FaceLayer"])
+            [layer setHidden:YES];
+    }
+    
+    // Create transform to convert from vide frame coordinate space to view coordinate space
+    CGAffineTransform t = [self affineTransformForVideoFrame:rect orientation:videoOrientation];
+    
+    for (int i = 0; i < faces.size(); i++) {
+        
+        CGRect faceRect;
+        faceRect.origin.x = faces[i].x;
+        faceRect.origin.y = faces[i].y;
+        faceRect.size.width = faces[i].width;
+        faceRect.size.height = faces[i].height;
+        
+        faceRect = CGRectApplyAffineTransform(faceRect, t);
+        
+        CALayer *featureLayer = nil;
+        
+        while (!featureLayer && (currentSublayer < sublayersCount)) {
+            CALayer *currentLayer = [sublayers objectAtIndex:currentSublayer++];
+            if ([[currentLayer name] isEqualToString:@"FaceLayer"]) {
+                featureLayer = currentLayer;
+                [currentLayer setHidden:NO];
+            }
+        }
+        
+        if (!featureLayer) {
+            // Create a new feature marker layer
+            featureLayer = [[CALayer alloc] init];
+            featureLayer.name = @"FaceLayer";
+            featureLayer.borderColor = [[UIColor redColor] CGColor];
+            featureLayer.borderWidth = 10.0f;
+            [self.view.layer addSublayer:featureLayer];
+        }
+        
+        featureLayer.frame = faceRect;
+    }
+    
+    [CATransaction commit];
+}
+
+- (CGAffineTransform)affineTransformForVideoFrame:(CGRect)videoFrame orientation:(AVCaptureVideoOrientation)videoOrientation
+{
+    CGSize viewSize = self.view.bounds.size;
+    NSString * const videoGravity = _videoPreviewLayer.videoGravity;
+    CGFloat widthScale = 1.0f;
+    CGFloat heightScale = 1.0f;
+    
+    // Move origin to center so rotation and scale are applied correctly
+    CGAffineTransform t = CGAffineTransformMakeTranslation(-videoFrame.size.width / 2.0f, -videoFrame.size.height / 2.0f);
+    
+    switch (videoOrientation) {
+        case AVCaptureVideoOrientationPortrait:
+            widthScale = viewSize.width / videoFrame.size.width;
+            heightScale = viewSize.height / videoFrame.size.height;
+            break;
+            
+        case AVCaptureVideoOrientationPortraitUpsideDown:
+            t = CGAffineTransformConcat(t, CGAffineTransformMakeRotation(M_PI));
+            widthScale = viewSize.width / videoFrame.size.width;
+            heightScale = viewSize.height / videoFrame.size.height;
+            break;
+            
+        case AVCaptureVideoOrientationLandscapeRight:
+            t = CGAffineTransformConcat(t, CGAffineTransformMakeRotation(M_PI_2));
+            widthScale = viewSize.width / videoFrame.size.height;
+            heightScale = viewSize.height / videoFrame.size.width;
+            break;
+            
+        case AVCaptureVideoOrientationLandscapeLeft:
+            t = CGAffineTransformConcat(t, CGAffineTransformMakeRotation(-M_PI_2));
+            widthScale = viewSize.width / videoFrame.size.height;
+            heightScale = viewSize.height / videoFrame.size.width;
+            break;
+    }
+    
+    // Adjust scaling to match video gravity mode of video preview
+    if (videoGravity == AVLayerVideoGravityResizeAspect) {
+        heightScale = MIN(heightScale, widthScale);
+        widthScale = heightScale;
+    }
+    else if (videoGravity == AVLayerVideoGravityResizeAspectFill) {
+        heightScale = MAX(heightScale, widthScale);
+        widthScale = heightScale;
+    }
+    
+    // Apply the scaling
+    t = CGAffineTransformConcat(t, CGAffineTransformMakeScale(widthScale, heightScale));
+    
+    // Move origin back from center
+    t = CGAffineTransformConcat(t, CGAffineTransformMakeTranslation(viewSize.width / 2.0f, viewSize.height / 2.0f));
+    
+    return t;
+}
+
+- (IBAction)toggleCamera:(id)sender
+{
+    if (self.camera == 0) {
+        self.camera = 1;
+    }
+    else
+    {
+        self.camera = 0;
+    }
+}
+
+- (void)setCamera:(NSInteger)camera
+{
+    if (camera != _camera)
+    {
+        _camera = camera;
+        
+        if (self.captureSession) {
+            NSArray* devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+            
+            [self.captureSession beginConfiguration];
+            
+            [self.captureSession removeInput:[[self.captureSession inputs] lastObject]];
+            
+            if (self.camera >= 0 && self.camera < [devices count]) {
+                self.captureDevice = [devices objectAtIndex:camera];
+            }
+            else {
+                self.captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+            }
+            
+            // Create device input
+            NSError *error = nil;
+            AVCaptureDeviceInput *input = [[AVCaptureDeviceInput alloc] initWithDevice:self.captureDevice error:&error];
+            if ( [self.captureSession canAddInput:input] ) {
+                [self.captureSession addInput:input];
+            } else {
+                NSLog(@"Could not add input port to capture session %@", self.captureSession);
+            }
+            
+            [_captureSession commitConfiguration];
+        }
+    }
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '7.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'FaceDetector' do
+pod 'OpenCV', '2.4.9'
+end
+
+target 'FaceDetectorTests' do
+
+end
+
